79567c0663e2d77fd55abcee3ccee9e6a3868f95
Victor <victor.henrique5800@gmail.com>
2023-12-06 09:43:59 -0300

    Implementando dashboard

--- task2/dashboard.py
+++ task2/dashboard.py
@@ -4,16 +4,43 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
-r = redis.Redis(host='localhost', port=6379, db=0)
+def getMockData():
+    data_dict = {
+        'percent-network-egress': 0.8,
+        'percent-memory-cache': 0.6,
+        'moving-avg-cpu1': 0.5,
+        'moving-avg-cpu2': 0.6,
+        'moving-avg-cpu3': 0.4,
+        'moving-avg-cpu4': 0.2,
+        'moving-avg-cpu5': 0.3,
+        'moving-avg-cpu6': 0.1,
+        'moving-avg-cpu7': 0.9,
+        'moving-avg-cpu8': 0.4,
+        'moving-avg-cpu9': 0.5,
+        'moving-avg-cpu10': 0.4,
+        'moving-avg-cpu11': 0.7,
+        'moving-avg-cpu12': 0.3,
+        'moving-avg-cpu13': 0.6,
+        'moving-avg-cpu14': 0.5,
+        'moving-avg-cpu15': 0.8,
+        'moving-avg-cpu16': 0.5,
+    }
 
-my_id = 0
-key = f"{my_id}-proj3-output"
-data = r.get(key)
+    return data_dict
 
-data = data.decode('utf-8')
+def getDataFromRedis():
+    r = redis.Redis(host='localhost', port=6379, db=0)
 
-data_dict = json.loads(data)
+    my_id = 0
+    key = f"{my_id}-proj3-output"
+    data = r.get(key)
 
+    data = data.decode('utf-8')
+
+    data_dict = json.loads(data)
+    return data_dict
+
+data_dict = getMockData()
 network_egress = data_dict['percent-network-egress']
 memory_cache = data_dict['percent-memory-cache']
 moving_cpu_avgs = []
