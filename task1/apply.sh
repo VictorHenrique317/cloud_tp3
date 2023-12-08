f3acb69944f4c3916053fa7278fa6af13cccae28
Victor Henrique <68289673+VictorHenrique317@users.noreply.github.com>
2023-12-08 13:12:50 -0300

    requirements

--- task1/apply.sh
+++ task1/apply.sh
@@ -1,3 +1,8 @@
+kubectl delete configmap pyfile
+kubectl delete configmap outputkey
+kubectl delete configmap requirementsfile
+kubectl delete deployment serverless-deployment-course
+
 kubectl apply -f pyfile.yaml
 kubectl apply -f outputkey.yaml
 kubectl apply -f requirementsfile.yaml
